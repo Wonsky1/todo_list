988e43524a3316828e77b7b550db4dcdfdce4fca
wonsky <pidborskii17@gmail.com>
2023-11-27 10:46:32 +0200

    Created urls for future project

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path, include
 
+from todo.views import (
+    TaskListView, TaskCreateView,
+    TagsListView, TagCreateView,
+    change_status
+)
+
+
 urlpatterns = [
     path("", include("todo.urls", namespace="todo")),
-
+    path("", TaskListView.as_view(), name="task-list"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("tags/", TagsListView.as_view(), name="tags-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
+
+app_name = "todo"
